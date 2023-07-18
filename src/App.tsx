216d4220970e793bff3663a2c8b91050541e742c
jostu1 <joshua.tu@kindredgroup.com>
2023-07-18 22:54:44 +1000

    feat(main): create some ant design Form demo

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+// import FormDemo from "./components/1_基本使用_FormDemo";
+// import FormDemo from "./components/2_表单调用_FormDemo";
+// import FormDemo from "./components/3_表单调用_ClassComp_FormDemo";
+// import FormDemo from "./components/4_表单布局_FormDemo";
+// import FormDemo from "./components/5_表单禁用_FormDemo(含一些输入类型用例)";
+// import FormDemo from "./components/6_必选样式_FormDemo";
+// import FormDemo from "./components/7_表单尺寸_FormDemo";
+import FormDemo from "./components/8_字段路径前缀_FormDemo";
+
+function App() {
+  return (
+    <>
+      <FormDemo />
+    </>
+  );
+}
+
+export default App;
